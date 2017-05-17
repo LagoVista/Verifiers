0bb37b2e81c06a57b086f70cc606ed5d04e8a284
Kevin D. Wolf <kevinw@software-logistics.com>
2017-05-17 13:04:37 -0400

    Full Commit

--- tests/LagoVista.IoT.Verifiers.Tests/ParseBinaryPayloadTests.cs
+++ tests/LagoVista.IoT.Verifiers.Tests/ParseBinaryPayloadTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LagoVista.IoT.Verifiers.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace LagoVista.IoT.Verifiers.Tests
 {
